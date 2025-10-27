36a0a56285582839e84ae4a75dfd2c71a65a5657
Nacho-Paz <82242852+Nacho-Paz@users.noreply.github.com>
2025-10-27 09:06:29 -0300

    Upload SQL Document

---
+++ AlmacenLibro SQL.sql
@@ -0,0 +1,23 @@
+-- create database almacenLibro;
+
+/*create table fecha(
+idFecha int not null primary key auto_increment,
+dia int not null,
+mes int not null,
+anio int not null
+);*/
+
+/*create table libro(
+idLibro int not null primary key auto_increment,
+isbn int not null,
+nombre varchar(50),
+autor varchar(50),
+editorial varchar(50),
+genero varchar (50),
+fechaPub int,
+foreign key (fechaPub) references fecha (idFecha)
+);*/
+
+-- select * from fecha;
+
+-- select * from libro;
